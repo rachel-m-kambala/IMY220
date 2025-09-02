69464b032239f968d48baaf298404bb1196b71df
rachel-m-kambala <rachel.m.kambala@gmail.com>
2025-09-03 01:54:51 +0200

    babel, package.json and index.js updated

---
+++ Deliverable_1/frontend/src/index.js
@@ -0,0 +1,12 @@
+//Mukaji Mweni Rachel Kambala u23559129 24
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
